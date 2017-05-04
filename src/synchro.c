ec87d5232838fd509c8a8e83d8dfe175f08c4dc6
Nolwenn Forey <nolwenn.forey@ensimag.grenoble-inp.fr>
2017-05-04 11:45:24 +0200

    3.3.1 fenetre texture

--- src/synchro.c
+++ src/synchro.c
@@ -8,18 +8,20 @@ bool fini;
 /* les variables pour la synchro, ici */
 
 sem_t *semaphore_taille_fenetre;
+sem_t *semaphore_fenetre_texture;
 
 // https://www.theora.org/doc/libtheora-1.0/structth__img__plane.html
 struct th_img_plane * TailleFenetre;
 
 
-
 void semaphores_init() {
 	sem_init(semaphore_taille_fenetre, 0, 0);
+	sem_init(semaphore_fenetre_texture, 0, 0);
 }
 
 void semaphores_destroy(){
 	sem_destroy(semaphore_taille_fenetre);
+	sem_destroy(semaphore_fenetre_texture);
 }
 
 /* l'implantation des fonctions de synchro ici */
@@ -34,9 +36,11 @@ void attendreTailleFenetre() {
 }
 
 void signalerFenetreEtTexturePrete() {
+	sem_post(semaphore_fenetre_texture);
 }
 
 void attendreFenetreTexture() {
+	sem_wait(semaphore_fenetre_texture);
 }
 
 void debutConsommerTexture() {
