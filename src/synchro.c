fd26df704a6e978aeef1cfb98b6d47b756f798e2
Benjamin Khenniche <benjamin.khenniche@ensimag.grenoble-inp.fr>
2017-05-04 11:36:30 +0200

    3.3.1 sem fenetre

--- src/synchro.c
+++ src/synchro.c
@@ -1,18 +1,36 @@
 #include "synchro.h"
 #include "ensitheora.h"
 
+#include <semaphore.h> 
 
 bool fini;
 
 /* les variables pour la synchro, ici */
 
+sem_t *semaphore_taille_fenetre;
+// https://www.theora.org/doc/libtheora-1.0/structth__img__plane.html
+th_ycbcr_buffer TailleFenetre;
 
+
+
+
+void semaphores_init() {
+	sem_init(semaphore_taille_fenetre, 0, 0);
+}
+
+void semaphores_destroy(){
+	sem_destroy(semaphore_taille_fenetre);
+}
 
 /* l'implantation des fonctions de synchro ici */
+
 void envoiTailleFenetre(th_ycbcr_buffer buffer) {
+	TailleFenetre =  buffer;
+	sem_post(semaphore_taille_fenetre);
 }
 
 void attendreTailleFenetre() {
+	sem_wait(semaphore_taille_fenetre);
 }
 
 void signalerFenetreEtTexturePrete() {
