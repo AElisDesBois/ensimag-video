2aa4a606b961a0c7b19de8ef72a116f4218225fb
Nolwenn Forey <nolwenn.forey@ensimag.grenoble-inp.fr>
2017-05-10 08:37:12 +0200

    3.3.2

--- src/synchro.c
+++ src/synchro.c
@@ -9,6 +9,10 @@ bool fini;
 
 sem_t *semaphore_taille_fenetre;
 sem_t *semaphore_fenetre_texture;
+sem_t *semaphore_texture_vide;
+sem_t *semaphore_texture_plein;
+pthread_mutex_t *mutex_consommer_texture;
+pthread_mutex_t *mutex_deposer_texture;
 
 // https://www.theora.org/doc/libtheora-1.0/structth__img__plane.html
 struct th_img_plane * TailleFenetre;
@@ -17,11 +21,15 @@ struct th_img_plane * TailleFenetre;
 void semaphores_init() {
 	sem_init(semaphore_taille_fenetre, 0, 0);
 	sem_init(semaphore_fenetre_texture, 0, 0);
+	sem_init(semaphore_texture_vide, 0, NBTEX);
+	sem_init(semaphore_texture_plein, 0, 0);
 }
 
 void semaphores_destroy(){
 	sem_destroy(semaphore_taille_fenetre);
 	sem_destroy(semaphore_fenetre_texture);
+	sem_destroy(semaphore_texture_vide);
+	sem_destroy(semaphore_texture_plein);
 }
 
 /* l'implantation des fonctions de synchro ici */
@@ -44,14 +52,22 @@ void attendreFenetreTexture() {
 }
 
 void debutConsommerTexture() {
+	sem_wait(semaphore_texture_plein);
+	pthread_mutex_lock(mutex_consommer_texture);
 }
 
 void finConsommerTexture() {
+	pthread_mutex_unlock(mutex_consommer_texture);
+	sem_post(semaphore_texture_vide);
 }
 
 
 void debutDeposerTexture() {
+	sem_wait(semaphore_texture_vide);
+	pthread_mutex_lock(mutex_deposer_texture);
 }
 
 void finDeposerTexture() {
+	pthread_mutex_unlock(mutex_deposer_texture);
+	sem_post(semaphore_texture_plein);
 }
